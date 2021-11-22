9d317c17ab73cb2bd28bfafd4326aa00f8b5cbc7
Rémi Heredero <remi@heredero.ch>
2021-11-22 15:06:38 +0100

    master

--- src/WordManager.java
+++ src/WordManager.java
@@ -63,27 +63,28 @@ public class WordManager {
         askLevel += "('h' for hardcore)";
         char level = Dialogs.getChar(askLevel);
         String s = "";
-        String[] word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots.csv"); // 331'782 mots
+        String[] word = loadList("src/mots.csv"); // 331'782 mots
+        // C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots.csv
 
         switch (level) {
             case 'b':
-                word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots_beginner.csv"); // 19 mots
+                word = loadList("src/mots_beginner.csv"); // 19 mots
                 break;
 
             case 'e':
-                word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots_easy.csv"); // 579 mots
+                word = loadList("src/mots_easy.csv"); // 579 mots
                 break;
 
             case 'm':
-                word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots_medium.csv"); // 4'872 mots
+                word = loadList("src/mots_medium.csv"); // 4'872 mots
                 break;
                 
             case 'd':
-                word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots_difficult.csv"); // 23'371 mots
+                word = loadList("src/mots_difficult.csv"); // 23'371 mots
                 break;
 
             case 'h':
-                word = loadList("C:/Users/remi/OneDrive/Documents/Cours/05-HEVS/S1fb/informatic/labo/vscode/HangMan/src/mots_hardcore.csv"); // 108'034 mots
+                word = loadList("src/mots_hardcore.csv"); // 108'034 mots
                 break;
 
             default:
